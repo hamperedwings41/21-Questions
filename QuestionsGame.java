0b0c02b094a1e25985e91815bb4f82ebda3251b2
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-02-11 09:25:09 -0600

    Add files via upload

--- QuestionsGame.java
+++ QuestionsGame.java
@@ -1,12 +1,54 @@
+import java.util.*;
 // This is a starter file for QuestionsGame.
 //
 // You should delete this comment and replace it with your class
 // header comment.
 
 public class QuestionsGame {
-    // Your code here
+    
+	Node root;
+	
+	public QuestionsGame()
+	{
+		root = null;
+	}
 
-    private static class QuestionNode {
-        // Your code here
-    }
+	public void read( String filename)
+	{
+		
+	}
+	
+	public void write( String filename)
+	{
+		
+	}
+	
+	public void askQuestions( String filename)
+	{
+		
+	}
+	
+	public boolean yesTo(String prompt)
+	{
+		return false;
+	}
+	
+		private class Node
+		{
+			Comparable data;
+			Node left;
+			Node right;
+
+			public Node( Comparable v, Node l, Node r)
+			{
+				data = v;
+				left = l;
+				right = r;
+			}
+			
+			public String toString()
+			{
+				return "" + data + " " + left + " " + right;
+			}
+		}
 }
